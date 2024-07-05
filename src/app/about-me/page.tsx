c6e43ba443bc2d7358fa05af8307925a66dc44e3
Jhuly Bastos <jhulytbds@gmail.com>
2024-07-05 11:17:48 -0300

    fix error

--- src/app/about-me/page.tsx
+++ src/app/about-me/page.tsx
@@ -5,7 +5,7 @@ export default function AboutMe() {
   return (
     <div className="w-full h-screen overflow-y-scroll bg-dark-10 grid grid-cols-1 lg:grid-cols-2 p-10">
       {informations.map((information) => (
-        <Card information={information} />
+        <Card information={information} key={information.id} />
       ))}
     </div>
   );
